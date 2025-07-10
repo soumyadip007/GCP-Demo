ba53ff5786ff1e88118437aa406cc6ca404921f2
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2025-07-10 15:18:13 +0530

    feat : change in endpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;;
 
-app.get('/product', (req, res) => {
+app.get('/account', (req, res) => {
   res.send(
     { 'data': "Hi I'm Product!" }
   );
