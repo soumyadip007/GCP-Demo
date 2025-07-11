3fddfe876dcfb6a10978d6376f39cb309b12e074
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2025-07-11 10:55:27 +0530

    feat: acc service

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;;
 
+// This is an account service
 app.get('/account', (req, res) => {
   res.send(
     { 'data': "Hi I'm Product!" }
